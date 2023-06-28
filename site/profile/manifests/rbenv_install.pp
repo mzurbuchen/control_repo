f31c1db040695d3f31a0b063e58c7460122bba40
Miro <miro.zurbuchen@gmail.com>
2023-06-28 11:07:46 +0200

    Update and rename rbenv.pp to rbenv_install.pp

---
+++ site/profile/manifests/rbenv_install.pp
@@ -0,0 +1,27 @@
+class profile::rbenv_install {
+  exec { 'install_rbenv':
+    command     => 'git clone https://github.com/rbenv/rbenv.git ~/.rbenv',
+    path        => '/usr/bin:/usr/sbin:/bin:/sbin',
+    user        => 'root',
+    creates     => '/root/.rbenv',
+    require     => Package['git'],
+  }
+
+  exec { 'configure_rbenv':
+    command     => 'echo -e \'export PATH="$HOME/.rbenv/bin:$PATH"\neval "$(rbenv init -)"\' >> /root/.bashrc',
+    path        => '/usr/bin:/usr/sbin:/bin:/sbin',
+    user        => 'root',
+    unless      => 'grep -Fxq "export PATH=\\"$HOME/.rbenv/bin:$PATH\\"" /root/.bashrc && grep -Fxq "eval \\"$(rbenv init -)\\"" /root/.bashrc',
+    require     => Exec['install_rbenv'],
+  }
+
+  exec { 'reload_bashrc':
+    command     => 'source /root/.bashrc',
+    path        => '/usr/bin:/usr/sbin:/bin:/sbin',
+    user        => 'root',
+    refreshonly => true,
+    require     => Exec['configure_rbenv'],
+  }
+}
+
+class { 'profile::rbenv_install': }
