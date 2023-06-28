edd2f6dd7fda990ba4f3374f998f0b75a6e5dc47
Miro <miro.zurbuchen@gmail.com>
2023-06-28 15:29:00 +0200

    Update development_tools.pp

--- site/profile/manifests/development_tools.pp
+++ site/profile/manifests/development_tools.pp
@@ -5,12 +5,17 @@ class profile::development_tools {
     # ensure => '1.9.4',
     # provider => 'yum'
   }
+
+  include profile::rbenv_install
+  include profile::ruby_install
+  include profile::bundler_install
+  
   # include profile::rbenv_install
   # class { 'profile::rbenv_install': }
 
-  node 'rbenv' {
-    include profile::rbenv_install
-    include profile::ruby_install
-    include profile::bundler_install
-  }
+  # node 'rbenv' {
+  #   include profile::rbenv_install
+  #   include profile::ruby_install
+  #   include profile::bundler_install
+  # }
 }
