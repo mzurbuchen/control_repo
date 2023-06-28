7ecada2aef33a69176a0c7868e6890c99e8bd149
Miro <miro.zurbuchen@gmail.com>
2023-06-28 10:29:45 +0200

    Create rbenv.pp

---
+++ site/profile/manifests/rbenv.pp
@@ -0,0 +1,28 @@
+class rbenv {
+  package { 'rbenv':
+    ensure   => installed,
+    provider => 'gem',
+    require  => Package['git'],
+  }
+
+  package { 'ruby-build':
+    ensure   => installed,
+    provider => 'gem',
+    require  => Package['rbenv'],
+  }
+
+  file { '/etc/profile.d/rbenv.sh':
+    ensure  => present,
+    content => 'export PATH="$HOME/.rbenv/bin:$PATH"',
+    require => Package['rbenv'],
+  }
+
+  exec { 'initialize-rbenv':
+    command     => '/bin/bash -l -c "rbenv init -"',
+    path        => '/bin:/usr/bin',
+    refreshonly => true,
+    require     => File['/etc/profile.d/rbenv.sh'],
+  }
+}
+
+include rbenv
