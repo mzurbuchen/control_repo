ab669f2c9120f3766f1cc3cb62c49c8c3db0258b
Miro <miro.zurbuchen@gmail.com>
2023-06-28 15:51:46 +0200

    Update ruby_install.pp

--- site/profile/manifests/ruby_install.pp
+++ site/profile/manifests/ruby_install.pp
@@ -1,22 +1,7 @@
 class profile::ruby_install {
-  exec { 'install_ruby_2.7.4':
-    command     => '/root/.rbenv/bin/rbenv install 2.7.4',
-    path        => '/usr/bin:/usr/sbin:/bin:/sbin:/root/.rbenv/bin',
-    user        => 'root',
-    creates     => '/root/.rbenv/versions/2.7.4',
+  include rbenv
+  rbenv::build::definition { '2.7.4':
+    ensure => present,
   }
-
-  exec { 'install_ruby_3.0.2':
-    command     => '/root/.rbenv/bin/rbenv install 3.0.2',
-    path        => '/usr/bin:/usr/sbin:/bin:/sbin:/root/.rbenv/bin',
-    user        => 'root',
-    creates     => '/root/.rbenv/versions/3.0.2',
-  }
-
-  exec { 'install_ruby_3.1.0':
-    command     => '/root/.rbenv/bin/rbenv install 3.1.0',
-    path        => '/usr/bin:/usr/sbin:/bin:/sbin:/root/.rbenv/bin',
-    user        => 'root',
-    creates     => '/root/.rbenv/versions/3.1.0',
-  }
+  rbenv::global { '2.7.4': }
 }
